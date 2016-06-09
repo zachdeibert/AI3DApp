a7ced153a4f38b4985fa93a6851babdeb8cdfd21
Zach Deibert <zachdeibert@gmail.com>
2016-06-09 10:25:42 -0500

    Added app inventor project files

---
+++ AI3DApp.aia/src/appinventor/ai_zachdeibert/AI3DApp/Rendering.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"150","Source":"Form","Properties":{"$Name":"Rendering","$Type":"Form","$Version":"19","AppName":"AI3DApp","ScreenOrientation":"portrait","ShowStatusBar":"False","Sizing":"Responsive","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Graphics","$Type":"Canvas","$Version":"10","Height":"-2","Width":"-2","Uuid":"-1511359255"},{"$Name":"Server","$Type":"Web","$Version":"4","Uuid":"1002052458"}]}}
+|#
