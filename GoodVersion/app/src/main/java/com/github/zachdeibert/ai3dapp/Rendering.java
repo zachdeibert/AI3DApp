be921d49f75a56809a2384014136eb83c4878469
Hanavan Kuhn <kuhnhan000@usd232.org>
2016-06-10 09:51:54 -0500

    Finished UI for the most part

--- GoodVersion/app/src/main/java/com/github/zachdeibert/ai3dapp/Rendering.java
+++ GoodVersion/app/src/main/java/com/github/zachdeibert/ai3dapp/Rendering.java
@@ -2,6 +2,8 @@ package com.github.zachdeibert.ai3dapp;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -9,6 +11,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.Image;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -31,14 +37,26 @@ import java.util.Random;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Rendering extends Activity {
+public class Rendering extends Activity implements SensorEventListener {
 
+    public double pitch;
+    public double yaw;
+    public double roll;
 
+    @Override
+    protected void onResume() {
+        SensorManager mSensorManager;
+        Sensor mSensor;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_rendering);
 
         Thread t = new Thread(new Runnable() {
@@ -74,34 +92,35 @@ public class Rendering extends Activity {
             public void run() {
                 Point size = new Point();
                 getWindowManager().getDefaultDisplay().getSize(size);
-                Vars.guid = request("/login", size.x, size.y);
+                Vars.guid = request("/login", size.x / 2, size.y / 2);
+                Bitmap bm = Bitmap.createBitmap(size.x / 2, size.y / 2, Bitmap.Config.ARGB_8888);
+                Canvas c = new Canvas(bm);
                 while (true)
                 {
-                    Random rnd = new Random();
-                    String csv = request("/frame/" + Vars.guid, rnd.nextDouble(), rnd.nextDouble(), rnd.nextDouble());
-                    Bitmap bm = Bitmap.createBitmap(size.x, size.y, Bitmap.Config.ARGB_8888);
-                    Canvas c = new Canvas(bm);
+                    String csv = request("/frame/" + Vars.guid, roll * 200, pitch * 200, 0);
                     c.drawBitmap(bm, 0, 0, null);
                     String[] pixeldata = csv.split("\n");
-                    for (String data : pixeldata) {
-                        String[] subdata = data.split(",");
-                        int x = Integer.valueOf(subdata[0]);
-                        int y = Integer.valueOf(subdata[1]);
-                        int r = Integer.valueOf(subdata[2]);
-                        int g = Integer.valueOf(subdata[3]);
-                        int b = Integer.valueOf(subdata[4]);
-                        Paint p = new Paint();
-                        p.setColor(Color.rgb(r, g, b));
-                        c.drawRect((float) x, (float) y, (float) x + 1, (float) y + 1, p);
-                    }
-                    final Bitmap bmf = bm;
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            ImageView p = (ImageView) findViewById(R.id.render);
-                            p.setImageDrawable(new BitmapDrawable(getResources(), bmf));
+                    if (pixeldata.length > 0) {
+                        for (String data : pixeldata) {
+                            String[] subdata = data.split(",");
+                            int x = Integer.valueOf(subdata[0]);
+                            int y = Integer.valueOf(subdata[1]);
+                            int r = Integer.valueOf(subdata[2]);
+                            int g = Integer.valueOf(subdata[3]);
+                            int b = Integer.valueOf(subdata[4]);
+                            Paint p = new Paint();
+                            p.setColor(Color.rgb(r, g, b));
+                            c.drawRect((float) x, (float) y, (float) x + 1, (float) y + 1, p);
                         }
-                    });
+                        final Bitmap bmf = bm;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ImageView p = (ImageView) findViewById(R.id.render);
+                                p.setImageDrawable(new BitmapDrawable(getResources(), bmf));
+                            }
+                        });
+                    }
                     //safeSleep(100);
                 }
             }
@@ -118,4 +137,17 @@ public class Rendering extends Activity {
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        pitch = event.values[0];
+        roll = event.values[1];
+        yaw = event.values[2];
+
+        //System.out.println("Pitch = " + pitch + ", Yaw = " + yaw + ", Roll = " + roll);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
